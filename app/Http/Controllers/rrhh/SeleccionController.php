2bbf5e657ea65ac506711457f52f499bb381d68e
renzojrq <renzorojasq@hotmail.com>
2018-05-01 22:53:14 -0400

    Modificada consulta de seleccion

--- app/Http/Controllers/rrhh/SeleccionController.php
+++ app/Http/Controllers/rrhh/SeleccionController.php
@@ -19,9 +19,15 @@ class SeleccionController extends Controller
         $area_id = $request->area_id;
         $cargo_id = $request->cargo_id;
 
-        $aspirantes = DB::table('aspirantes')
-            ->join('vacantes', 'aspirantes.vacante_id', '=', 'vacantes.vacante_id')
-            ->where('vacantes.sucursal_id', $sucursal_id)
+        $aspirantes = DB::table('sucursales')
+            ->join('vacantes', 'sucursales.sucursal_id', '=', 'vacantes.sucursal_id')
+            //->join('areas', 'vacantes.area_id', '=', 'areas.area_id')
+            ->join('aspirantes', 'vacantes.vacante_id', '=', 'aspirantes.vacante_id')
+            ->where([
+                ['sucursales.sucursal_id', $sucursal_id],
+                ['vacantes.area_id', $area_id],
+                ['vacantes.cargo_id', $cargo_id]
+            ])            
             //->where([
                 //['aspirantes.sucursal_id', '=', $sucursal_id],
                 //['vacantes.area_id', '=', $area_id],
